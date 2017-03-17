18aeb179972d07b040c056d99f1a480706787eff
handler <925616096@qq.com>
2017-03-17 17:23:15 +0800

    sun  jax-ws  webservic

--- handler-webservice/src/handler/server/Server.java
+++ handler-webservice/src/handler/server/Server.java
@@ -10,6 +10,7 @@ import javax.xml.ws.Endpoint;
 public class Server {
 
 	/**
+	 * 使用SunJAX-WS 2中Endpoint.publish进行发布
 	 * java中使用WebService须首先将服务端的服务根据描述生成相应的WSDL文件，并将应用及此WSDL文件放入HTTP服务�?
 	 * 借助java服务工具根据WSDL文件生成客户端代码；
 	 * 此代码的作用是将产生的对象请求信息分装成标准的SOAP格式数据，并发�?�请求服务端�?
