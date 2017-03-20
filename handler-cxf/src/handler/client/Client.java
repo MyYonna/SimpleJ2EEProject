02b17d8019b016bb797683760b6cd5c93fb75250
handler <925616096@qq.com>
2017-03-20 18:32:56 +0800

    包含了cxf与spring-cxf的客户端

--- handler-cxf/src/handler/client/Client.java
+++ handler-cxf/src/handler/client/Client.java
@@ -4,6 +4,7 @@ import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
 import org.apache.cxf.jaxws.endpoint.dynamic.JaxWsDynamicClientFactory;
 
 import handler.service.Business;
+import handler.service.BusinessService;
 
 public class Client {
 
@@ -38,6 +39,35 @@ public class Client {
 		}
 
 		 //输出调用结果
+		//spring集成的webservice
+		//静态调用
+		 JaxWsProxyFactoryBean soapFactoryBean1 = new JaxWsProxyFactoryBean();
+
+		 soapFactoryBean1.setWsdlURL("http://localhost:8081/handler-cxf-spring/ws/businessService?wsdl");
+
+		 soapFactoryBean1.setServiceClass(BusinessService.class);
+
+		 Object o1 = soapFactoryBean1.create();
+
+		 BusinessService business1 = (BusinessService)o1; 
+		 
+		 String res1 = business1.echo("i have send a static  message");
+		 
+		 System.out.println(res1);
+		
+		 /////////////////////////////////////////////////动态调用客户端////////////////////////////////////
+		 JaxWsDynamicClientFactory dcf1 =JaxWsDynamicClientFactory.newInstance();
+
+		 org.apache.cxf.endpoint.Client client1 = dcf1.createClient("http://localhost:8081/handler-cxf-spring/ws/businessService?wsdl");
+
+		 //sayHello 为接口中定义的方法名称  张三为传递的参数   返回一个Object数组
+		try {
+			Object[] objects1 = client1.invoke("echo","i have send a dynamic message");
+			System.out.println(objects1[0].toString()); 
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 }
